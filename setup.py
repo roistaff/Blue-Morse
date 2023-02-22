d093f4961478c5c181d110ef227b4dcb1f490eea
roistaff <118814428+roistaff@users.noreply.github.com>
2023-02-22 17:39:56 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name='blue-morse',
     version='0.1',
     packages=['blue_morse'],
+    description='Bluetooth button morse app',
+    author='Roi Staff',
+    author_email='roistaff1983@gmail.com'
     entry_points={
         'console_scripts': [
             'blue-morse = blue_morse.blue_morse:autostart'
